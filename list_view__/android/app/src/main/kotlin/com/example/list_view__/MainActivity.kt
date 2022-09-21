68fd5c37f48d597509f64c920835216ed363eeae
Tien Nguyen Tran <tiennt@tech.est-rouge.com>
2022-09-21 23:47:48 +0700

    add code list

---
+++ list_view__/android/app/src/main/kotlin/com/example/list_view__/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_view__
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ list_view__/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:list_view__/view/pages/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Coffee Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
